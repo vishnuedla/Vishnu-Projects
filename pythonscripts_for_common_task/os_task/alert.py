05bcd90f4c83e79bb9d9c59e6a8cfdc8444f9278
vishnuedla <edlavishnu@aol.com>
2026-06-11 23:22:50 +0530

    Update email alert sender and recipient details

--- pythonscripts_for_common_task/os_task/alert.py
+++ pythonscripts_for_common_task/os_task/alert.py
@@ -4,18 +4,18 @@ from email.message import EmailMessage
 def email_alert(pid):
     msg = EmailMessage()
     msg['Subject'] = f'Process with PID 287 is not running'
-    msg['From'] = 'edlavishnu@aol.com'
+    msg['From'] = 'SENDER_EMAI'
     msg.set_content(
         "Hello,\n\n"
        f"The process with PID {pid} is not running.\n\n"
         "Regards,\nAWS Monitoring Script"
     )
-    msg['To'] = 'edlavishnu2000@gmail.com'
+    msg['To'] = 'Receive_email'
     server = smtplib.SMTP('smtp.aol.com',587)
     server.starttls()
     server.login(
-           'edlavishnu@aol.com',
-           'xotp pyqk cgwg ayga')
+           'SENDER_EMAIL',
+           'APP PASSWORD')
     server.send_message(msg)
     server.quit()
     print("email successfully sent ")
